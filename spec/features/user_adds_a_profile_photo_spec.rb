7793ce5a9336448adb695cbb206704131c1ed209
kennpat <kennedypr@gmail.com>
2015-01-05 15:26:56 -0500

    Fixing Hound comments

--- spec/features/user_adds_a_profile_photo_spec.rb
+++ spec/features/user_adds_a_profile_photo_spec.rb
@@ -1,27 +1,25 @@
-require 'rails_helper'
-require 'carrierwave/test/matchers'
+require "rails_helper"
+require "carrierwave/test/matchers"
 
-feature 'user registers', %Q{
-  As a new user I would like to add a profile picture
-} do
+feature "user registers and adds a profile photo" do
 
   # Acceptance Criteria:
   # * I must be able to upload a file and be able to see it after upload
 
-  scenario 'user uploads a profile pic during registration' do
+  scenario "user uploads a profile pic during registration" do
     visit new_user_registration_path
 
-    fill_in 'Email', with: 'john@example.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    attach_file "Profile photo", File.join(Rails.root, 'spec/support/images/example.jpg')
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    attach_file "Profile photo", File.join(Rails.root, "spec/support/images/example.jpg")
 
-    click_button 'Sign up'
+    click_button "Sign up"
 
-    expect(page).to have_content('Welcome! You have signed up successfully.')
-    expect(page).to have_content('Sign Out')
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+    expect(page).to have_content("Sign Out")
     # expect(page).to have_css("img", src: @user.profile_photo_url)
-    expect(page).to have_css("img[src*='uploads']")
+    expect(page).to have_css("img[src*="uploads"]")
   end
 
 end
