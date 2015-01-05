6238d7b3921290fa60d28e3ad43aa407e2c77ce3
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-01-05 13:07:39 -0500

    Address pull request comments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,35 +2,24 @@ class VotesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @vote = Vote.new(choice: params[:vote][:choice])
+    @review = Review.find(params[:review_id])
+    @vote = @review.votes.build(vote_params)
     @vote.user = current_user
-    @vote.review_id = params[:review_id]
     if @vote.save
       redirect_to @vote.review.weather, notice: "Thank you for voting."
     else
-      redirect_to @vote.review.weather,
-        notice: "You have already voted on that review"
+      flash[:notice] = "You have already voted on that review"
+      redirect_to @vote.review.weather
     end
   end
 
   def update
     @vote = Vote.find(params[:id])
     @weather = @vote.review.weather
-    if @vote.user != current_user
-      redirect_to @weather
-    end
-
-    if @vote.choice == "up"
-      @vote.choice = "down"
-    else
-      @vote.choice = "up"
-    end
-
-    if @vote.save
-      redirect_to @weather, notice: "Vote Changed"
-    else
-      redirect_to @weather
-    end
+    redirect_to @weather if @vote.user != current_user
+    @vote.choice == "up" ? choice = "down" : choice = "up"
+    flash[:notice] = "Vote Changed" if @vote.update_attributes(choice: choice)
+    redirect_to @weather
   end
 
   def destroy
@@ -43,5 +32,11 @@ class VotesController < ApplicationController
     else
       redirect_to @weather
     end
+  end
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:user_id, :review_id, :choice)
   end
 end
