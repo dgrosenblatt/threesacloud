4212239dd6210ae91400d1b960e76571019a215d
kennpat <kennedypr@gmail.com>
2015-01-07 14:39:25 -0500

    added more to the test for the emailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def review_alert(user)
-    mail to: user.email, subject: "A review has been posted to your weather"
+    @user = user
+    mail to: @user.email, subject: "A review has been posted to your weather"
     
   end
 end
--- spec/features/create_review_spec.rb
+++ spec/features/create_review_spec.rb
@@ -8,26 +8,49 @@ feature "User wants to add a new review to a weather submission " do
 
     end
 
-    scenario "user properly enters a new review" ,focus: true do
-      weather = FactoryGirl.create(:weather)
+    scenario "user properly enters a new review" , %Q{
+      As a registered user
+      I want post a review
+      So that I can share my opinion with other people
+      }, focus: true do
+      @weather = FactoryGirl.create(:weather)
       ActionMailer::Base.deliveries = []
 
-      visit "/weathers/#{weather.id}"
+      visit "/weathers/#{@weather.id}"
 
       fill_in "Comment", with: "Man it's cold!"
       select(2, :from => "Rating")
 
       click_on("Submit")
 
-      expect(ActionMailer::Base.deliveries.size).to eql(1)
-      last_email = ActionMailer::Base.deliveries.last
-      expect(last_email).to deliver_to(User.find(weather.user_id))
-      expect(last_email).to have_subject("A review has been posted to your weather")
-
-
       expect(page).to have_content("Man it's cold!")
       expect(page).to have_content("2")
     end
+
+    scenario "user properly enters a new review" , %Q{
+      As a registered user
+      I want to be notified when someone posts a review
+      So that I can read that review
+      }, focus: true do
+        @weather = FactoryGirl.create(:weather)
+        ActionMailer::Base.deliveries = []
+
+        visit "/weathers/#{@weather.id}"
+
+        fill_in "Comment", with: "Man it's cold!"
+        select(2, :from => "Rating")
+
+        click_on("Submit")
+
+        expect(ActionMailer::Base.deliveries.size).to eql(1)
+        last_email = ActionMailer::Base.deliveries.last
+        expect(last_email).to deliver_to(User.find(@weather.user_id))
+        expect(last_email).to have_subject("A review has been posted to your weather")
+        expect(last_email).to have_body_text("Greetings, #{User.find(@weather.user_id).email}
+          Your submitted weather observation has been commented on by another user.
+          Please visit your submission to see the review.")
+
+      end
 
     scenario "user enters invalid info" do
       weather = FactoryGirl.create(:weather)
