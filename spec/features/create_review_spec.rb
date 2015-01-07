6442f6796e7d23d698206b5566b1e17a74dd781a
kennpat <kennedypr@gmail.com>
2015-01-07 15:00:38 -0500

    addressed hound comments

--- spec/features/create_review_spec.rb
+++ spec/features/create_review_spec.rb
@@ -8,7 +8,7 @@ feature "User wants to add a new review to a weather submission " do
 
     end
 
-    scenario "user properly enters a new review" , %Q{
+    scenario "user properly enters a new review" , %{
       As a registered user
       I want post a review
       So that I can share my opinion with other people
@@ -19,7 +19,7 @@ feature "User wants to add a new review to a weather submission " do
       visit "/weathers/#{@weather.id}"
 
       fill_in "Comment", with: "Man it's cold!"
-      select(2, :from => "Rating")
+      select(2, from: "Rating")
 
       click_on("Submit")
 
@@ -27,7 +27,7 @@ feature "User wants to add a new review to a weather submission " do
       expect(page).to have_content("2")
     end
 
-    scenario "user properly enters a new review" , %Q{
+    scenario "user properly enters a new review", %{
       As a registered user
       I want to be notified when someone posts a review
       So that I can read that review
@@ -38,7 +38,7 @@ feature "User wants to add a new review to a weather submission " do
         visit "/weathers/#{@weather.id}"
 
         fill_in "Comment", with: "Man it's cold!"
-        select(2, :from => "Rating")
+        select(2, from: "Rating")
 
         click_on("Submit")
 
@@ -46,8 +46,9 @@ feature "User wants to add a new review to a weather submission " do
         last_email = ActionMailer::Base.deliveries.last
         expect(last_email).to deliver_to(User.find(@weather.user_id))
         expect(last_email).to have_subject("A review has been posted to your weather")
-        expect(last_email).to have_body_text("Greetings, #{User.find(@weather.user_id).email}
-          Your submitted weather observation has been commented on by another user.
+        expect(last_email).to have_body_text("Greetings,
+          #{User.find(@weather.user_id).email} Your submitted weather
+          observation has been commented on by another user.
           Please visit your submission to see the review.")
 
       end
